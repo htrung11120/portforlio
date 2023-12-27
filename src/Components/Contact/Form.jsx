365396a48e8eb10a55a79d408d6f957de48979f9
htrung11120 <htrung11120@gmail.com>
2023-12-26 23:14:45 -0500

    add contact sending email

--- src/Components/Contact/Form.jsx
+++ src/Components/Contact/Form.jsx
@@ -1,38 +1,41 @@
 export default function Form() {
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    window.location.href = "http://localhost:5173/";
+  };
   return (
     <div className="  flex h-auto w-9/12 flex-col items-center  justify-center md:w-[600px] ">
       <p className=" mt-10 text-4xl text-white md:mt-14 md:text-5xl">
         Send me a message!
       </p>
       <form
-        action="https://formsubmit.co/htrung11120@gmail.com"
+        action="https://formspree.io/f/mleqrylv"
         method="POST"
         className=" md:text-md ml-14 mt-12 flex  w-10/12 flex-col gap-12 text-xs"
         onSubmit={handleSubmit}
       >
-        <div className="flex w-full  justify-between gap-8  md:flex-row">
+        <div className="flex w-full flex-wrap justify-between gap-8  md:flex-row">
           <input
             type="text"
             name="senderName"
             placeholder="Your Name"
-            className="h-10 w-full grow rounded-md pl-1 md:pl-4"
+            className="h-10 grow rounded-md pl-1 md:pl-4"
             required
           />
           <input
             type="email"
             name="senderEmail"
             placeholder="Your email"
-            className="h-10 w-full grow rounded-md pl-1 md:pl-4"
+            className="h-10  grow rounded-md pl-1 md:pl-4"
             required
           />
+          <input
+            type="text"
+            name="subject"
+            placeholder="Subject..."
+            className="h-10 w-full grow rounded-md pl-1 md:pl-4"
+          />
         </div>
-        <input
-          type="text"
-          name="subject"
-          placeholder="Subject..."
-          className="h-10 w-full grow rounded-md pl-1 md:pl-4"
-        />
+
         <textarea
           name="message"
           id=""
@@ -42,6 +45,7 @@ export default function Form() {
           className=" rounded-md p-4 "
           required
         ></textarea>
+
         <button
           type="submit"
           className="h-10 w-full self-center rounded-md bg-white text-center md:h-16"
